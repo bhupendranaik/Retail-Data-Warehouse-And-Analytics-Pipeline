2acb616b200ddb92c7e10c810a9ab4887484978e
Bhupendra Naik <bhupendra.naik265@gmail.com>
2025-04-16 15:39:50 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -9,15 +9,12 @@ def lambda_handler(event, context):
     bucket = s3_info['bucket']['name']
     key = s3_info['object']['key']
     
-    # Build dynamic input and output paths
     input_path = f"s3://{bucket}/{key}"
     
-    # Optional: You can customize output_path logic if needed
     output_path = "s3://retail-cleaned-output123/parquet_output/"
 
-    # Start Glue job with arguments
     response = glue.start_job_run(
-        JobName='your-glue-job-name',  # Replace with your actual Glue job name
+        JobName='your-glue-job-name', 
         Arguments={
             '--input_path': input_path,
             '--output_path': output_path
